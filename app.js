1d69771db1b1bfdf68d829f3663a366bddb2f193
Cycle9898 <cycle9898@gmail.com>
2024-02-21 18:29:37 +0100

    added express and base code to run an API

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello Word !");
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello Word ! 👋"));
+
+app.listen(port, () =>
+	console.log(`The app is running on : http://localhost:${port}`)
+);
