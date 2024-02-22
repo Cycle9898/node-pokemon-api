080b2dbf0beafcb05e82a1f287b0be36aa3ce062
Cycle9898 <cycle9898@gmail.com>
2024-02-22 15:41:41 +0100

    added a test GET endpoint with a param in URL + changed message on main endpoint

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello Word ! 👋"));
+app.get("/", (req, res) => res.send("Pokemons API is up and running !"));
+
+app.get("/api/pokemons/:id", (req, res) => {
+	const id = req.params.id;
+	res.send(`Vous avez demandé le Pokémon numéro ${id}`);
+});
 
 app.listen(port, () =>
 	console.log(`The app is running on : http://localhost:${port}`)
