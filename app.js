fe872f3aa5f9dbebb3afa5ee7192521173652313
Cycle9898 <cycle9898@gmail.com>
2024-02-22 15:57:30 +0100

    added mocked data + updated test endpoint with it

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { pokemons } from "./mocked_data/mock-pokemon.js";
 
 const app = express();
 const port = 3000;
@@ -6,8 +7,9 @@ const port = 3000;
 app.get("/", (req, res) => res.send("Pokemons API is up and running !"));
 
 app.get("/api/pokemons/:id", (req, res) => {
-	const id = req.params.id;
-	res.send(`Vous avez demandé le Pokémon numéro ${id}`);
+	const id = parseInt(req.params.id);
+	const chosenPokemon = pokemons.find(pokemon => pokemon.id === id);
+	res.send(`Vous avez demandé le Pokémon ${chosenPokemon.name}`);
 });
 
 app.listen(port, () =>
