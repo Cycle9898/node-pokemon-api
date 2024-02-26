eb4fbf892db1ce02e1e2ae4c0819336e0939f768
Cycle9898 <cycle9898@gmail.com>
2024-02-26 16:03:43 +0100

    added POST endpoint to add a new pokemon + created a helper to get unique ID + added middleware to parse request body

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import express from "express";
 import morgan from "morgan";
 import favicon from "serve-favicon";
-import { success } from "./utils/helper.js";
+import { getUniqueId, success } from "./utils/helper.js";
 import { pokemons } from "./mocked_data/mock-pokemon.js";
 
 const app = express();
 const port = 3000;
 
-app.use(favicon("./assets/favicon.ico")).use(morgan("dev"));
+app.use(favicon("./assets/favicon.ico")).use(morgan("dev")).use(express.json());
 
 app.get("/", (req, res) => res.send("Pokemons API is up and running !"));
 
@@ -15,6 +15,14 @@ app.get("/api/pokemons", (req, res) => {
 	const message = `La liste des ${pokemons.length} Pokémons a bien été récupérée.`;
 
 	res.json(success(message, pokemons));
+});
+
+app.post("/api/pokemons", (req, res) => {
+	const id = getUniqueId(pokemons);
+	const pokemonCreated = { ...req.body, ...{ id, created: new Date() } };
+	pokemons.push(pokemonCreated);
+	const message = `Le Pokémon ${pokemonCreated?.name} a bien été créé.`;
+	res.json(success(message, pokemonCreated));
 });
 
 app.get("/api/pokemons/:id", (req, res) => {
