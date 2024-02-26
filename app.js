4857497f4da2796a812947f775534cfb409c1e6c
Cycle9898 <cycle9898@gmail.com>
2024-02-26 17:03:26 +0100

    added PUT endpoint to modify a pokemon + stored pokemons in another variable to permit CRUD operations to work in memory

--- app.js
+++ app.js
@@ -7,30 +7,42 @@ import { pokemons } from "./mocked_data/mock-pokemon.js";
 const app = express();
 const port = 3000;
 
+let pokemonsArray = [...pokemons];
+
 app.use(favicon("./assets/favicon.ico")).use(morgan("dev")).use(express.json());
 
 app.get("/", (req, res) => res.send("Pokemons API is up and running !"));
 
 app.get("/api/pokemons", (req, res) => {
-	const message = `La liste des ${pokemons.length} Pokémons a bien été récupérée.`;
+	const message = `La liste des ${pokemonsArray.length} Pokémons a bien été récupérée.`;
 
-	res.json(success(message, pokemons));
+	res.json(success(message, pokemonsArray));
 });
 
 app.post("/api/pokemons", (req, res) => {
-	const id = getUniqueId(pokemons);
+	const id = getUniqueId(pokemonsArray);
 	const pokemonCreated = { ...req.body, ...{ id, created: new Date() } };
-	pokemons.push(pokemonCreated);
+	pokemonsArray.push(pokemonCreated);
 	const message = `Le Pokémon ${pokemonCreated?.name} a bien été créé.`;
 	res.json(success(message, pokemonCreated));
 });
 
 app.get("/api/pokemons/:id", (req, res) => {
 	const id = parseInt(req.params.id);
-	const chosenPokemon = pokemons.find(pokemon => pokemon.id === id);
+	const chosenPokemon = pokemonsArray.find(pokemon => pokemon.id === id);
 	const message = "Un Pokémon a bien été trouvé";
 
 	res.json(success(message, chosenPokemon));
+});
+
+app.put("/api/pokemons/:id", (req, res) => {
+	const id = parseInt(req.params.id);
+	const pokemonUpdated = { ...req.body, id };
+	pokemonsArray = pokemonsArray.map(pokemon =>
+		pokemon.id === id ? pokemonUpdated : pokemon
+	);
+	const message = `Le Pokémon ${pokemonUpdated.name} a bien été modifié.`;
+	res.json(success(message, pokemonUpdated));
 });
 
 app.listen(port, () =>
