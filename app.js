320bf0013ad6080d7d5bba6fceaf23b9c2f8352e
Cycle9898 <cycle9898@gmail.com>
2024-02-29 18:47:02 +0100

    cleaned useless imports and variables in app.js + deleted utils folder with unused helpers

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ import morgan from "morgan";
 import favicon from "serve-favicon";
 import { initDb, testDatabaseConnection } from "./src/database/sequelize.js";
 import { pokemonRouter } from "./src/Routes/pokemonsRoutes.js";
-import { getUniqueId, success } from "./src/utils/helper.js";
-import { pokemons } from "./src/database/mocked_data/mock-pokemon.js";
-
-// copy of Pokemon mocked data (will be deleted with real endpoints)
-let pokemonsArray = [...pokemons];
 
 // Express app
 const app = express();
