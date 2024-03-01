57bc45c6db22299792873b5285cb989d900b7f23
Cycle9898 <cycle9898@gmail.com>
2024-03-01 18:39:18 +0100

    added 404 and 500 statuses responses on all Pokemon endpoints to handle common API errors

--- src/controllers/pokemonsControllers.js
+++ src/controllers/pokemonsControllers.js
@@ -3,44 +3,91 @@ import { Pokemon } from "../database/sequelize.js";
 export const getAllPokemons = (req, res) => {
 	Pokemon.findAll().then(pokemons => {
 		const message = `La liste des ${pokemons.length} Pokémons a bien été récupérée.`;
-		res.json({ message, data: pokemons });
+		res.json({ message, data: pokemons }).catch(error => {
+			const message =
+				"La liste des Pokémons n'a pas pu être récupérée. Veuillez réessayer dans quelques instants.";
+			res.status(500).json({ message, data: error });
+		});
 	});
 };
 
 export const getPokemon = (req, res) => {
-	Pokemon.findByPk(req.params.id).then(pokemon => {
-		const message = `Le Pokémon ${pokemon.name} a bien été récupéré.`;
-		res.json({ message, data: pokemon });
-	});
+	Pokemon.findByPk(req.params.id)
+		.then(pokemon => {
+			if (pokemon === null) {
+				const message =
+					"Le Pokémon demandé n'existe pas. Veuillez vérifier l'identifiant.";
+				return res.status(404).json({ message });
+			}
+
+			const message = `Le Pokémon ${pokemon.name} a bien été récupéré.`;
+			res.json({ message, data: pokemon });
+		})
+		.catch(error => {
+			const message =
+				"Le Pokémon n'a pas pu être récupéré. Veuillez réessayer dans quelques instants.";
+			res.status(500).json({ message, data: error });
+		});
 };
 
 export const addPokemon = (req, res) => {
-	Pokemon.create(req.body).then(pokemon => {
-		const message = `Le Pokémon ${pokemon.name} a bien été créé.`;
-		res.json({ message, data: pokemon });
-	});
+	Pokemon.create(req.body)
+		.then(pokemon => {
+			const message = `Le Pokémon ${pokemon.name} a bien été créé.`;
+			res.json({ message, data: pokemon });
+		})
+		.catch(error => {
+			const message =
+				"Le Pokémon n'a pas pu être ajouté. Veuillez réessayer dans quelques instants.";
+			res.status(500).json({ message, data: error });
+		});
 };
 
 export const updatePokemon = (req, res) => {
 	const id = req.params.id;
 	Pokemon.update(req.body, {
 		where: { id }
-	}).then(_ => {
-		Pokemon.findByPk(id).then(pokemon => {
-			const message = `Le Pokémon ${pokemon.name} a bien été modifié.`;
-			res.json({ message, data: pokemon });
+	})
+		.then(_ => {
+			return Pokemon.findByPk(id).then(pokemon => {
+				if (pokemon === null) {
+					const message =
+						"Le Pokémon demandé n'existe pas. Veuillez vérifier l'identifiant.";
+					return res.status(404).json({ message });
+				}
+
+				const message = `Le Pokémon ${pokemon.name} a bien été modifié.`;
+				res.json({ message, data: pokemon });
+			});
+		})
+		.catch(error => {
+			const message =
+				"Le Pokémon n'a pas pu être modifié. Veuillez réessayer dans quelques instants.";
+			res.status(500).json({ message, data: error });
 		});
-	});
 };
 
 export const deletePokemon = (req, res) => {
-	Pokemon.findByPk(req.params.id).then(pokemon => {
-		const pokemonDeleted = pokemon;
-		Pokemon.destroy({
-			where: { id: pokemon.id }
-		}).then(_ => {
-			const message = `Le Pokémon avec l'identifiant n°${pokemonDeleted?.id} a bien été supprimé.`;
-			res.json({ message, data: pokemonDeleted });
+	Pokemon.findByPk(req.params.id)
+		.then(pokemon => {
+			if (pokemon === null) {
+				const message =
+					"Le Pokémon demandé n'existe pas. Veuillez vérifier l'identifiant.";
+				return res.status(404).json({ message });
+			}
+
+			const pokemonDeleted = pokemon;
+
+			return Pokemon.destroy({
+				where: { id: pokemon.id }
+			}).then(_ => {
+				const message = `Le Pokémon avec l'identifiant n°${pokemonDeleted?.id} a bien été supprimé.`;
+				res.json({ message, data: pokemonDeleted });
+			});
+		})
+		.catch(error => {
+			const message =
+				"Le Pokémon n'a pas pu être supprimé. Veuillez réessayer dans quelques instants.";
+			res.status(500).json({ message, data: error });
 		});
-	});
 };
